eb3e6b93723554fc2ef752ffbb220d51415f4d18
LGJ420 <leegj420@gmail.com>
2024-10-08 14:01:15 +0900

    프로젝트 시작

---
+++ src/main/java/com/example/cattalk/CattalkApplication.java
@@ -0,0 +1,13 @@
+package com.example.cattalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CattalkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CattalkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cattalk/CattalkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cattalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CattalkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
