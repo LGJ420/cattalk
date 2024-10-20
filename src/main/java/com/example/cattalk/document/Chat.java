90a94adbec8981661be60197c62d7a4b397167d4
LGJ420 <leegj420@gmail.com>
2024-10-20 15:36:20 +0900

    도큐먼트 TTL 설정 제거

--- src/main/java/com/example/cattalk/document/Chat.java
+++ src/main/java/com/example/cattalk/document/Chat.java
@@ -3,7 +3,6 @@ package com.example.cattalk.document;
 import java.time.LocalDateTime;
 
 import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import lombok.Data;
@@ -20,6 +19,10 @@ public class Chat {
     private String receiver;
     private String room;
 
-    @Indexed(expireAfterSeconds = 86400)  // 24시간 후 자동 삭제
+    // TTL을 설정해주었으나
+    // 어째서인지 우분투에 설치한 몽고DB에서는
+    // 캡드컬렉션과 TTL을 동시에 설정하지 못한다고함
+    // 그래서 TTL 설정을 지웠음
+    // @Indexed(expireAfterSeconds = 86400)  // 24시간 후 자동 삭제
     private LocalDateTime createdAt;
 }
